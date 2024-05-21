b78023225d397c402e954922a34ab3090251ede6
skyDe <sky_de1991@outlook.com>
2024-05-21 11:45:19 -0800

    nabbar / hero / highlights components initialized and added to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { Hero } from "./components/Hero";
+import Highlights from "./components/Highlights";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <main className="app">
-      <h2>app</h2>
+      <Navbar />
+      <Hero />
+      <Highlights />
     </main>
   );
 }
