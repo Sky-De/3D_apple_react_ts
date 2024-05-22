e1ecaab2368099304ab9f3b1cf3c01501d6c1301
skyDe <sky_de1991@outlook.com>
2024-05-22 07:17:28 -0800

    Navbar is done

---
+++ src/components/Icon.tsx
@@ -0,0 +1,26 @@
+import { ComponentProps } from "react";
+
+type OverrideIconProps<T, TOmited, TMandatory> = Omit<T, keyof TOmited> &
+  TMandatory;
+type IconProps = OverrideIconProps<
+  ComponentProps<"img">,
+  { width: number; height: number; alt: string },
+  { alt: string; src: string; className?: string }
+>;
+
+const IconImage = ({ className, src, alt, ...props }: IconProps) => {
+  return (
+    <img
+      src={src}
+      width={18}
+      height={22}
+      className={`cursor-pointer sm:w-[22px] sm:h-[26px] ${
+        className && className
+      }`}
+      {...props}
+      alt={alt}
+    />
+  );
+};
+
+export default IconImage;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,29 @@
+import { appleImg, bagImg, searchImg } from "../utils";
+import { v4 as uuid } from "uuid";
+import IconImage from "./Icon";
+import { navLists } from "../constants";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <IconImage alt="apple" src={appleImg} />
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {/* add these to constants */}
+          {navLists.map((navItem) => (
+            <div
+              className="capitalize px-5 cursor-pointer text-sm text-gray hover:text-white transition-all"
+              key={uuid()}
+            >
+              {navItem}
+            </div>
+          ))}
+        </div>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <IconImage alt="search" src={searchImg} />
+          <IconImage alt="bag" src={bagImg} />
+        </div>
+      </nav>
+    </header>
+  );
 };
