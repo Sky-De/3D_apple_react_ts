45ad63d1627ca825bddfe55efbe171454727ac2d
skyDe <sky_de1991@outlook.com>
2024-06-22 05:21:52 -0800

    three js added / 3D model config added / needs refactor / needs ModelView to be done for next step

---
+++ src/components/ModelView.tsx
@@ -0,0 +1,13 @@
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  item,
+  size,
+}) => {
+  return <div>ModelView {index}</div>;
+};
+
+export default ModelView;
