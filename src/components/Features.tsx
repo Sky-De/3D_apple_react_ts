334a71fd40b449fdbd7ae0b7c33c7662fd0ee3fd
skyDe <sky_de1991@outlook.com>
2024-06-25 11:34:30 -0800

    features component initialized

---
+++ src/components/Features.tsx
@@ -0,0 +1,10 @@
+const Features = () => {
+  return (
+    <section className="h-full common-padding bg-zinc relative overflow-hidden">
+      Features
+      {/* add features of product here */}
+    </section>
+  );
+};
+
+export default Features;
