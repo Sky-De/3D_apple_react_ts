90cebf2069cad605d5fd0a41db009a10dba12b2f
skyDe <sky_de1991@outlook.com>
2024-07-01 05:58:26 -0800

    ErrorBoundry.tsx added and done / added to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import ErrorBoundary from "./ErrorBoundry.tsx";
 
 //...
 import * as Sentry from "@sentry/react";
@@ -27,6 +28,8 @@ Sentry.init({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
