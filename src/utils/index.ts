997d51efed267f5fb1abf92a2819c88dfb38e0f5
skyDe <sky_de1991@outlook.com>
2024-07-01 05:55:03 -0800

    Add maintenance.svg / Maintain.tsx for using as ErrorBoundry fallback -- style add to index.css -- maintenance.svg imported and exported from src/utils/index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -29,6 +29,8 @@ import explore2 from "/assets/images/explore2.jpg";
 import chip from "/assets/images/chip.jpeg";
 import frame from "/assets/images/frame.png";
 
+import Maintenance from "/assets/maintenance.svg";
+
 export const heroVideo = hmv;
 export const smallHeroVideo = smallmv;
 export const highlightFirstVideo = highlightFirstmv;
@@ -55,3 +57,5 @@ export const explore1Img = explore1;
 export const explore2Img = explore2;
 export const chipImg = chip;
 export const frameImg = frame;
+
+export const MaintenanceSvg = Maintenance;
