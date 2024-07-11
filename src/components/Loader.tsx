0967e8e8407f88ea53abf58246ea8bf9cec50b90
skyDe <sky_de1991@outlook.com>
2024-07-11 08:32:44 -0800

    testId added to src/components/Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,8 +3,14 @@ import { Html } from "@react-three/drei";
 const Loader = () => {
   return (
     <Html>
-      <div className="absolute top-0 left-0 w-full h-full">
-        <div className="w-6 h-6 rounded-full border-4 border-t-orange-500 animate-spin"></div>
+      <div
+        data-testid="loaderCon"
+        className="absolute top-0 left-0 w-full h-full"
+      >
+        <div
+          data-testid="loader"
+          className="w-6 h-6 rounded-full border-4 border-t-orange-500 animate-spin"
+        ></div>
       </div>
     </Html>
   );
